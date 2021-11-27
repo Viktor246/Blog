d98fba7dd2f8396523699cae556d3de6fa3a1e26
Viktor Antunovic <antunovic.viktor@gmail.com>
2021-11-27 18:59:06 +0100

    Created posts migration, created posts table in DB, removed static code brom blog index and made it so it loads real data from database, made relationshipo beetwin posts and users table 1 user has many posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,23 +9,27 @@
         </h1>
     </div>
 </div>
-<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py.15 borded-b border-gray-200">
-    <div>
-        <img src="https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_960_720.jpg" alt="">
+
+@foreach ($posts as $post)
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py.15 borded-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_960_720.jpg" alt="">
+        </div>
+        <div>
+            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                {{ $post->title }}
+            </h2>
+            <span class="text-gray-500">
+                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </span>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+            </p>
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Keep Reading
+            </a>
+        </div>
     </div>
-    <div>
-        <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            Learn how to write Laravel code
-        </h2>
-        <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">Code with Viktor</span>, 1 day ago
-        </span>
-        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus deserunt explicabo eveniet voluptates veritatis, repellendus officia cumque dignissimos architecto quibusdam eaque dolor veniam tempora ullam ipsam doloribus praesentium accusantium in.
-        </p>
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-            Keep Reading
-        </a>
-    </div>
-</div>
+@endforeach
+
 @endsection
