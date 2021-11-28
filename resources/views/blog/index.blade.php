291cd1e0f407c29152bca4d40c8c8308ce633a5d
Viktor Antunovic <antunovic.viktor@gmail.com>
2021-11-28 13:51:07 +0100

    Made a function to create new posts from form

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,8 +10,24 @@
     </div>
 </div>
 
+@if (session()->has('message'))
+    <div class="w-4/5 m-auto mt-10 pl-2">
+        <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4">
+            {{ session()->get('message') }}
+        </p>
+    </div>    
+@endif
+
+@if (Auth::check())
+    <div class="pt-15 w-4/5 m-auto">
+        <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+            Create Post
+        </a>
+    </div>
+@endif
+
 @foreach ($posts as $post)
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py.15 borded-b border-gray-200">
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 borded-b border-gray-200">
         <div>
             <img src="https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_960_720.jpg" alt="">
         </div>
